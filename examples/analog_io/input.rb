5dd354c4c843b8b5a88c808829d2a69cc333d4a5
vickash <vickashmahabir@gmail.com>
2023-07-06 17:06:49 -0400

    Improve the AnalogIO::Input example

--- examples/analog_io/input.rb
+++ examples/analog_io/input.rb
@@ -1,52 +1,55 @@
 #
-# This is an example of how to use the sensor class
-# You must register data callbacks and have the main thread
-# sleep or in someway keep running or your program
-# will exit before any callbacks can be called
+# This example shows how to use your board's analog-to-digital-converter(ADC) pins,
+# through the AnalogIO::Input class. ADC inputs can be connected to sensors
+# which produce a variable output voltage, such as light dependent resistors,
+# or a simple temperature sensor like the TMP36.
 #
 require 'bundler/setup'
 require 'denko'
 
 board = Denko::Board.new(Denko::Connection::Serial.new)
-sensor = Denko::AnalogIO::Sensor.new(pin: 'A0', board: board)
+input = Denko::AnalogIO::Input.new(pin: :A0, board: board)
 
-# Single read with block as callback. Blocks main thread.
-# Callback fires only once then is removed automatically.
-sensor.read { |value| puts "#{Time.now} Single read: #{value}" }
+# Single read that blocks the main thread. When a value is received, the given
+# code block runs only once, and then the main thread continues.
+input.read { |value| puts "#{Time.now} Single read #1: #{value}" }
 
-# Poll the sensor every 1 second with block as callback. Does not block main thread.
-# Callback fires every time data is received until #stop is called.
-sensor.poll(1) { |value| puts "#{Time.now} Polling: #{value}" }
-sleep 5
+# Read (poll) the input every half second. This happens in a separate thread, 
+# so sleep the main thread for a while to get some values.
+# Given code block is added as a callback, and runs every time a value is received.
+input.poll(0.5) { |value| puts "#{Time.now} Polling: #{value}" }
+sleep 3
 
-# Stop polling. Automatically removes the callback from the #poll block.
-sensor.stop
+# Stop polling. Automatically removes the #poll callback.
+input.stop
 
-# Continuous listen with block as callback. Fires every time data is received until #stop is called.
-sensor.listen { |value| puts "#{Time.now} Listening: #{value}" }
+# Listening is similar to polling. The board reads the input and sends the value
+# every time interval, except it's keeping time. We only send the initial command,
+# not one for each read. Smaller intervals like 32 milliseconds are possible.
+# Powers of 2 from 1 to 128 are supported for listener intervals.
+input.listen(32) { |value| puts "#{Time.now} Listening: #{value}" }
 sleep 0.5
 
-# Stop listening. Automatically removes the callback from the #listen block.
-sensor.stop
+# Stop listening. Automatically removes the #listen callback.
+input.stop
 
-# Add a persistent callback.
-sensor.on_data { |value| puts "#{Time.now} Persistent callback: #{value}" }
+# This adds a persistent callback, which runs no matter how a read happens.
+# It will not be removed by #stop.
+input.on_data { |value| puts "#{Time.now} Persistent callback: #{value}" }
 
-# Add a callback with a custom key.
-sensor.on_data(:test) { |value| puts "#{Time.now} Keyed callback: #{value}"}
+# This is a persistent callback with a custom key.
+input.on_data(:test) { |value| puts "#{Time.now} Keyed callback: #{value}"}
 
-# Single read again. Block given fires only once. Callbacks added with #on_data fire also.
-sensor.read { |value| puts "#{Time.now} Single read again: #{value}" }
+# If we do a single read, the two persistent callbacks, and the block given, should run once each.
+input.read { |value| puts "#{Time.now} Single read #2: #{value}" }
 
-# Continuous listen. Block fires each time. Callbacks added with #on_data continue to fire.
-sensor.listen { |value| puts "#{Time.now } Listening again: #{value}" }
-sleep 0.5
-
-# Stop listening. Automatically removes the callback from the #listen block.
-sensor.stop
+# If we listen, the two persistent callbacks, and the block given should run many times.
+input.listen(8) { |value| puts "#{Time.now } Listening again: #{value}" }
+sleep 0.125
+input.stop
 
 # Remove callbacks keyed with :test.
-sensor.remove_callbacks(:test)
+input.remove_callbacks(:test)
 
 # Remove all callbacks.
-sensor.remove_callbacks
+input.remove_callbacks
