3de76249eda41221d7fd1c5b23846788d42b6480
vickash <vickashmahabir@gmail.com>
2023-07-06 16:19:26 -0400

    Keyword args for IRTransmitter#emit

--- lib/denko/pulse_io/ir_transmitter.rb
+++ lib/denko/pulse_io/ir_transmitter.rb
@@ -3,7 +3,7 @@ module Denko
     class IRTransmitter
       include Behaviors::OutputPin
 
-      def emit(pulses=[], options={})
+      def emit(pulses=[], frequency: 38)
         if pulses.length > 256 || pulses.length < 1
           raise ArgumentError, 'wrong number of IR pulses (expected 1 to 256)'
         end
@@ -14,7 +14,6 @@ module Denko
           raise ArgumentError, 'pulse too long (max 65535 ms)' if pulse > 65535
         end
 
-        frequency = options[:frequency] || 38
         board.infrared_emit(pin, frequency, pulses)
       end
     end
