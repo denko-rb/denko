3b05fe13e7ccfe4bfabae06d1ed270da41ab62ee
vickash <vickashmahabir@gmail.com>
2023-07-20 14:46:36 -0400

    Make ADS1118 more generic to support 1115

--- examples/analog_io/ads1118.rb
+++ examples/analog_io/ads1118.rb
@@ -14,12 +14,12 @@ bus = Denko::SPI::Bus.new(board: board)
 # bus = Denko::SPI::BitBang.new(board: board, pins: SPI_BIT_BANG_PINS)
 
 # Connect chip select/enable pin of the ADS1118 to pin 9.
-ads1118 = Denko::AnalogIO::ADS1118.new(bus: bus, pin: 9)
+ads = Denko::AnalogIO::ADS1118.new(bus: bus, pin: 9)
 
 # Helper method so readings look nice.
 def print_reading(name, raw, voltage)
   print "#{Time.now.strftime '%Y-%m-%d %H:%M:%S'} - "
-  print "#{name.rjust(12, " ")} | "
+  print "#{name.rjust(18, " ")} | "
   print "Raw: #{raw.to_s.rjust(6, " ")} | "
   print "Voltage: "
   print ("%.10f" % voltage).rjust(13, " ")
@@ -30,7 +30,7 @@ end
 # Read the ADS1118 internal temperature sensor.
 # This always uses the 128 SPS mode, and there is no polling method for it.
 # 
-temperature = ads1118.temperature_read
+temperature = ads.temperature_read
 puts "ADS1118 Temperature: #{temperature} \xC2\xB0C"
 puts
 
@@ -41,7 +41,7 @@ puts
 #
 # Note: This is the only way to use continuous mode. Subcomponents always use one-shot.
 #
-ads1118.read([0b10000001, 0b10001011]) do |reading|
+ads.read([0b10000001, 0b10001011]) do |reading|
   voltage = reading * 0.0001875
   print_reading("Direct", reading, voltage)
 end
@@ -49,26 +49,25 @@ end
 #
 # Or use its BoardProxy interface, adding subcomponents as if it were a Board.
 # The key adc: can substitute for board: when intializing AnalogIO::Input.
-#
 # Gain and sample rate bitmasks can be found in the datasheet.
 #
 # Input on pin 0, with pin 1 as differential negative input, and 6.144 V full range.
-diff_input = Denko::AnalogIO::Input.new(adc: ads1118, pin: 0, negative_pin: 1, gain: 0b000)
+diff_input = Denko::AnalogIO::Input.new(adc: ads, pin: 0, negative_pin: 1, gain: 0b000)
 
 # Input on pin 2 with no negative input (single ended), and 1.024V full range.
 # Ths one uses a 8 SPS rate, essentially 16x oversampling compared to the default 128.
-single_input = Denko::AnalogIO::Input.new(adc: ads1118, pin: 2, gain: 0b011, sample_rate: 0b000)
+single_input = Denko::AnalogIO::Input.new(adc: ads, pin: 2, gain: 0b011, sample_rate: 0b000)
 
 # Poll the differential input every second.
 diff_input.poll(1) do |reading|
   voltage = reading * diff_input.volts_per_bit
-  print_reading("Differential", reading, voltage)
+  print_reading("Differential A1-A0", reading, voltage)
 end
 
 # Poll the single ended input every 2 seconds.
 single_input.poll(2) do |reading|
   voltage = reading * single_input.volts_per_bit
-  print_reading("Single", reading, voltage)
+  print_reading("Single A2-GN", reading, voltage)
 end
 
 sleep
--- lib/denko/analog_io/ads1118.rb
+++ lib/denko/analog_io/ads1118.rb
@@ -45,12 +45,16 @@ module Denko
         0b111 => [3, nil],
       }
 
-      # Config register values on startup.
-      CONFIG_DEFAULT = [0x05, 0x8B]
+      # Config register values on startup. MSB-first.
+      # Matches datasheet. Same as: [0x05, 0x8B] or [5, 139]
+      CONFIG_STARTUP = [0b00000101, 0b10001011]
 
       # Base config bytes to mask settings into. Not same as default.
+      # MSB bits 0 and 7 set to enable single-shot mode.
+      # LSB bit 3 set to enable pullup resistor on MISO pin.
+      # LSB bit 1 set to flag valid data in the NOP bits.
       BASE_MSB = 0b10000001
-      BASE_LSB = 0b00001011
+      BASE_LSB = 0b00001010
 
       def after_initialize(options={})
         super(options)
@@ -64,7 +68,7 @@ module Denko
         @active_gain  = nil
 
         # Set register bytes to default and write to device.
-        @config_register = CONFIG_DEFAULT.dup
+        @config_register = CONFIG_STARTUP.dup
         spi_write(@config_register)
 
         # Enable BoardProxy callbacks.
@@ -82,7 +86,7 @@ module Denko
         spi_read(2)
       end
 
-      # Pack the 2 bytes back into a string, then unpack as big-endian int16.
+      # Pack the 2 bytes back into a string, then unpack as big-endian signed int16.
       def pre_callback_filter(message)
         bytes = message.split(",").map { |b| b.to_i }
         bytes.pack("C*").unpack("s>")[0]
