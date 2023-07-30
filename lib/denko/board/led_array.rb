39c7cff5f0395c6b14f26946b8c4a824a40c4b4b
vickash <vickashmahabir@gmail.com>
2023-07-30 10:04:26 -0400

    Implement longer WS2812 and fix a bug when passing length to board

--- lib/denko/board/led_array.rb
+++ lib/denko/board/led_array.rb
@@ -1,17 +1,14 @@
 module Denko
   class Board
-    def show_ws2812(pin, pixel_buffer)
-      length = pixel_buffer.length
-
+    def show_ws2812(pin, pixel_count, pixel_buffer)
       # Settings are blank for now.
       settings = pack :uint8, [0, 0, 0, 0]
       
-      # Limit to 100 pixels (3-bytes each) for now
-      packed_pixels = pack :uint8, pixel_buffer, max: 300
-
+      packed_pixels = pack :uint8, pixel_buffer
+      
       write_and_halt Message.encode command: 19,
                                     pin: pin,
-                                    value: length,
+                                    value: pixel_count,
                                     aux_message: settings + packed_pixels
     end
   end
