2f315a1809b052697a9d266ce7f5240b2a938e2c
vickash <vickashmahabir@gmail.com>
2024-03-02 21:43:03 -0400

    More temp/press/humidity print improvements

--- examples/sensor/neat_tph_readings.rb
+++ examples/sensor/neat_tph_readings.rb
@@ -6,21 +6,25 @@ def print_tph_reading(reading)
   # Time
   print "#{Time.now.strftime '%Y-%m-%d %H:%M:%S'} - "
   
+  elements = []
+
   # Temperature
-  formatted_temp = reading[:temperature].to_f.round(2).to_s.ljust(5, '0')
-  print "Temperature: #{formatted_temp} \xC2\xB0C"
+  if reading[:temperature]
+    formatted_temp = reading[:temperature].to_f.round(2).to_s.ljust(5, '0')
+    elements << "Temperature: #{formatted_temp} \xC2\xB0C"
+  end
   
   # Pressure
   if reading[:pressure]
     formatted_pressure = (reading[:pressure] / 101325).round(5).to_s.ljust(7, '0')
-    print " | Pressure: #{formatted_pressure} atm"
+    elements << "Pressure: #{formatted_pressure} atm"
   end
   
   # Humidity  
   if reading[:humidity]
     formatted_humidity = reading[:humidity].round(2).to_s.ljust(5, '0')
-    print " | Humidity: #{formatted_humidity} %"
+    elements << "Humidity: #{formatted_humidity} %"
   end
   
-  puts
+  puts elements.join(" | ")
 end
