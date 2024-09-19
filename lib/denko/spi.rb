f5832bd37761e7fe96b26eb89b48c89cbb036b48
vickash <vickashmahabir@gmail.com>
2024-09-19 16:17:53 -0400

    Autoload SPI::Peripheral::Core too

--- lib/denko/spi.rb
+++ lib/denko/spi.rb
@@ -3,6 +3,7 @@ module Denko
     autoload :Bus,            "#{__dir__}/spi/bus"
     autoload :BitBang,        "#{__dir__}/spi/bit_bang"
     module Peripheral
+      autoload :Core,         "#{__dir__}/spi/peripheral"
       autoload :SinglePin,    "#{__dir__}/spi/peripheral"
       autoload :MultiPin,     "#{__dir__}/spi/peripheral"
     end
