5214c5a91f49fafa76438ba4d1d254a4b36c0450
vickash <vickashmahabir@gmail.com>
2025-04-12 15:05:25 -0400

    Better autoloading for Display

--- lib/denko/display.rb
+++ lib/denko/display.rb
@@ -1,8 +1,8 @@
 module Denko
   module Display
     autoload :HD44780,  "#{__dir__}/display/hd44780"
-    require "#{__dir__}/display/canvas"
-    require "#{__dir__}/display/mono_oled"
+    autoload :Canvas,   "#{__dir__}/display/canvas"
+    autoload :MonoOLED, "#{__dir__}/display/mono_oled"
     autoload :SSD1306,  "#{__dir__}/display/ssd1306"
     autoload :SH1106,   "#{__dir__}/display/sh1106"
     autoload :SH1107,   "#{__dir__}/display/sh1107"
