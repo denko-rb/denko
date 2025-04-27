6a4fb93394b457840863c34fb84afb7875d06a85
vickash <vickashmahabir@gmail.com>
2025-04-27 15:41:22 -0400

    Clarify 128x32 OLED example

--- examples/display/oled_128x32.rb
+++ examples/display/oled_128x32.rb
@@ -1,18 +1,19 @@
 #
-# Generic example for 128x64 OLEDs, usually only SSD1306.
-# Also covers the one built into the Lolin ESP32-S2 Pico
+# Generic example for 128x29 OLEDs, usually only SSD1306 over I2C.
+# Also covers the one built into the Lolin ESP32-S2 Pico.
 #
 require 'bundler/setup'
 require 'denko'
 
 board = Denko::Board.new(Denko::Connection::Serial.new)
 
-# Modules usually have reset always tied high.
-# Lolin ESP32-P2 has it connected to pin 18. Do it manually.
+# Modules usually have reset permanently tied high.
+# Lolin ESP32-S2 has it connected to pin 18. Do it manually.
 # reset = Denko::DigitalIO::Output.new(board: board, pin: 18)
 # reset.high
 
 bus    = Denko::I2C::Bus.new(board: board, index: 0)
+# bus = Denko::I2C::BitBang.new(board: board, pins: {scl: 4, sda: 5})
 oled   = Denko::Display::SSD1306.new(bus: bus, width: 128, height: 32, rotate: true)
 canvas = oled.canvas
 
