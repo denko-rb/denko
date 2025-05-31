8e3400df59ec8b7ef66125c4941ecb20fac6c299
vickash <vickashmahabir@gmail.com>
2025-05-31 17:22:02 -0400

    Firmware CLI: cleanup comments and help info

--- lib/denko_cli/targets.rb
+++ lib/denko_cli/targets.rb
@@ -23,11 +23,11 @@ class DenkoCLI::Generator
     # Infrared library does not support the UNO R4.
     ra4m1: STANDARD_PACKAGES - [:ir_out],
 
-    # ESP8266 uses an IR library specific to it.
+    # ESP8266 & ESP32
     esp8266: STANDARD_PACKAGES,
     esp32:   STANDARD_PACKAGES,
 
-    # RP2040 can't use WS2812 yet.
+    # RP2040
     rp2040: STANDARD_PACKAGES,
   }
 end
