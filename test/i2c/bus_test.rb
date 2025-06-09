4f52ff1c4ecff29e0097302a31d0fb448ff50193
vickash <vickashmahabir@gmail.com>
2025-06-09 01:16:48 -0400

    I2C::Bus test: call it "bus" instead of "part"

--- test/i2c/bus_test.rb
+++ test/i2c/bus_test.rb
@@ -9,24 +9,24 @@ class I2CBusTest < Minitest::Test
     @board ||= BoardMock.new
   end
 
-  def part
-    @part ||= Denko::I2C::Bus.new(board: board)
+  def bus
+    @bus ||= Denko::I2C::Bus.new(board: board)
   end
 
   def peripheral
-    @peripheral ||= I2CPeripheralBase.new(bus: part, address: 0x30)
+    @peripheral ||= I2CPeripheralBase.new(bus: bus, address: 0x30)
   end
 
   def test_initialize
-    assert_equal part.found_devices, []
-    assert_equal 0, part.i2c_index
-    refute_nil part.callbacks[:bus_controller]
+    assert_equal bus.found_devices, []
+    assert_equal 0, bus.i2c_index
+    refute_nil bus.callbacks[:bus_controller]
 
-    part2 = Denko::I2C::Bus.new(board: board, index: 10)
-    assert_equal 10, part2.i2c_index
+    bus2 = Denko::I2C::Bus.new(board: board, index: 10)
+    assert_equal 10, bus2.i2c_index
 
-    part3 = Denko::I2C::Bus.new(board: board, i2c_index: 11)
-    assert_equal 11, part3.i2c_index
+    bus3 = Denko::I2C::Bus.new(board: board, i2c_index: 11)
+    assert_equal 11, bus3.i2c_index
   end
 
   def test_search
@@ -35,29 +35,29 @@ class I2CBusTest < Minitest::Test
 
     mock = Minitest::Mock.new.expect :call, nil, [0]
     board.stub(:i2c_search, mock) do
-      part.search
+      bus.search
     end
     mock.verify
 
-    assert_equal part.found_devices, [0x30, 0x32]
+    assert_equal bus.found_devices, [0x30, 0x32]
   end
 
   def test_search_with_array_response
-    board.inject_component_update(part, [0, 0x30, 0x32])
-    part.search
-    assert_equal [0x30, 0x32], part.found_devices
+    board.inject_component_update(bus, [0, 0x30, 0x32])
+    bus.search
+    assert_equal [0x30, 0x32], bus.found_devices
   end
 
   def test_search_empty_results
     board.inject_read_for_i2c(0, ":")
-    part.search
-    assert_equal [], part.found_devices
+    bus.search
+    assert_equal [], bus.found_devices
   end
 
   def test_write
     mock = Minitest::Mock.new.expect :call, nil, [0, 0x30, [0x01, 0x02], 100000, false]
     board.stub(:i2c_write, mock) do
-      part.write 0x30, [0x01, 0x02]
+      bus.write 0x30, [0x01, 0x02]
     end
     mock.verify
   end
@@ -67,7 +67,7 @@ class I2CBusTest < Minitest::Test
 
     mock = Minitest::Mock.new.expect :call, nil, [0, 0x32, 0x03, 6, 100000, false]
     board.stub(:i2c_read, mock) do
-      part.read 0x32, 0x03, 6
+      bus.read 0x32, 0x03, 6
     end
     mock.verify
   end
@@ -77,17 +77,17 @@ class I2CBusTest < Minitest::Test
 
     mock = Minitest::Mock.new.expect :call, nil, [0, 0x32, 0x03, 6, 100000, false]
     board.stub(:i2c_read, mock) do
-      part.read 0x32, 0x03, 6
+      bus.read 0x32, 0x03, 6
     end
     mock.verify
   end
 
   def test_read_without_register
-    board.inject_component_update(part, "48-255,127")
+    board.inject_component_update(bus, "48-255,127")
 
     mock = Minitest::Mock.new.expect :call, nil, [0, 0x30, nil, 2, 100000, false]
     board.stub(:i2c_read, mock) do
-      part.read 0x30, nil, 2
+      bus.read 0x30, nil, 2
     end
     mock.verify
   end
@@ -96,8 +96,8 @@ class I2CBusTest < Minitest::Test
     mock = Minitest::Mock.new.expect :call, nil, [[255, 127]]
 
     peripheral.stub(:update, mock) do
-      part.send(:update, "48-255,127")
-      part.send(:update, "50-128,0")
+      bus.send(:update, "48-255,127")
+      bus.send(:update, "50-128,0")
     end
     mock.verify
   end
@@ -106,8 +106,8 @@ class I2CBusTest < Minitest::Test
     mock = Minitest::Mock.new.expect :call, nil, [[255, 127]]
 
     peripheral.stub(:update, mock) do
-      part.send(:update, [0x30, 255, 127])
-      part.send(:update, [0x32, 128, 0])
+      bus.send(:update, [0x30, 255, 127])
+      bus.send(:update, [0x32, 128, 0])
     end
     mock.verify
   end
@@ -117,37 +117,37 @@ class I2CBusTest < Minitest::Test
     # Mock should not receive any calls
 
     peripheral.stub(:update, mock) do
-      part.send(:update, "49-255,127")  # Different address
+      bus.send(:update, "49-255,127")  # Different address
     end
     mock.verify
   end
 
   def test_handles_empty_data_gracefully
-    part.send(:update, "48-")
-    part.send(:update, "48")
+    bus.send(:update, "48-")
+    bus.send(:update, "48")
   end
 
   def test_handles_malformed_string_data
-    part.send(:update, "invalid-data")
-    part.send(:update, "")
-    part.send(:update, nil)
+    bus.send(:update, "invalid-data")
+    bus.send(:update, "")
+    bus.send(:update, nil)
   end
 
   def test_same_address_fails
     peripheral
-    assert_raises { I2CPeripheralBase.new(bus: part, address: 0x30) }
+    assert_raises { I2CPeripheralBase.new(bus: bus, address: 0x30) }
   end
 
   # Should split up Subcomponents behavior and test there?
   def test_component_management
-    count = part.components.length
-    new_peripheral = I2CPeripheralBase.new(bus: part, address: 0x40)
+    count = bus.components.length
+    new_peripheral = I2CPeripheralBase.new(bus: bus, address: 0x40)
 
-    assert_equal count+1, part.components.length
-    assert_includes part.components, new_peripheral
+    assert_equal count+1, bus.components.length
+    assert_includes bus.components, new_peripheral
   end
 
   def test_found_devices_not_writable
-    assert_raises { part.found_devices = nil }
+    assert_raises { bus.found_devices = nil }
   end
 end
