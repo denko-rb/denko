59a7c961fc5c13b3d60edddd4ee6e691cc2cb0cb
vickash <vickashmahabir@gmail.com>
2025-07-21 15:57:57 -0400

    Reader : bare minimum Mutex usage for internal thread safety

--- lib/denko/behaviors/reader.rb
+++ lib/denko/behaviors/reader.rb
@@ -7,8 +7,9 @@ module Denko
       READ_WAIT_TIME = 0.001
 
       after_initialize do
-        @read_type = :idle
-        @read_result = nil
+        @read_type    = :idle
+        @read_result  = nil
+        @state_mutex  = Denko.mruby? ? Denko::MutexStub.new : Mutex.new
       end
 
       # Define #_read in including class to update the component's state.
@@ -27,28 +28,19 @@ module Denko
       # of peripherals, like digital pin level, enviro sensor reading etc.
       #
       def read_using(reader_method, &block)
-        sleep READ_WAIT_TIME while (@read_type != :idle)
+        @state_mutex.lock
+          @read_type = :regular
+          reader_method.call
+          sleep READ_WAIT_TIME while (@read_type != :idle)
+          block.call(@read_result) if block_given?
+        @state_mutex.unlock
 
-        @read_type = :regular
-        reader_method.call
-
-        sleep READ_WAIT_TIME while (@read_type != :idle)
-        block.call(@read_result) if block_given?
         @read_result
       end
 
-      #
       # Default read method to be called by user.
-      #
       def read(&block)
-        sleep READ_WAIT_TIME while (@read_type != :idle)
-
-        @read_type = :regular
-        _read
-
-        sleep READ_WAIT_TIME while (@read_type != :idle)
-        block.call(@read_result) if block_given?
-        @read_result
+        read_using(self.method(:_read), &block)
       end
 
       #
@@ -59,12 +51,12 @@ module Denko
         # Can't guarantee read order.
         raise StandardError, "#read_raw unavailable while listening" if @listening
 
-        sleep READ_WAIT_TIME while (@read_type != :idle)
+        @state_mutex.lock
+          @read_type = :raw
+          reader_method.call
+          sleep READ_WAIT_TIME while (@read_type != :idle)
+        @state_mutex.unlock
 
-        @read_type = :raw
-        reader_method.call
-
-        sleep READ_WAIT_TIME while (@read_type != :idle)
         @read_result
       end
 
@@ -74,9 +66,17 @@ module Denko
       #   - For :raw reads see #read_raw
       #
       def update(data)
+        # If a read is in progress, let that method lock and unlock @state_mutex.
+        # Only lock/unlock here if no read was requested. Probably a listener.
+        read_in_progress = @read_type != :idle
+
+        @state_mutex.lock unless read_in_progress
+
         @read_result = (@read_type == :raw) ? data : super(data)
         @read_type = :idle
         @read_result
+
+        @state_mutex.unlock unless read_in_progress
       end
     end
   end
