73586d7858c4657315d2f656d8fe4830382bd3e4
vickash <vickashmahabir@gmail.com>
2026-04-02 18:15:05 -0400

    Lifecycle : Improve documentation, minor refactoring

--- lib/denko/behaviors/lifecycle.rb
+++ lib/denko/behaviors/lifecycle.rb
@@ -35,66 +35,61 @@ module Denko
     #
     module Lifecycle
       # @private
-      CALLBACK_METHODS = [:before_initialize, :after_initialize]
+      CALLBACK_METHODS = %i[before_initialize after_initialize]
 
-      # Automatically add callback registration class methods to including classes
-      def self.included(base); base.extend ClassMethods; end
+      # Defines callback methods in including classes.
+      def self.included(base)
+        base.extend ClassMethods
+      end
 
       # @!parse extend ClassMethods
       module ClassMethods
         private
-
-        # @!group Callback DSL
         # @!visibility public
 
-        # Registers the given block as a callback to run before main body of {Component#initialize}.
+        # Adds given block as callback to run before main body of {Component#initialize}.
+        # These are hereditary, running in **bottom-up order:** callbacks from descendents first, then ancestors.
         #
+        # @api private
         # @yield callback to run
         # @return [Array<Proc>] all callbacks defined using `.before_initialize` for this class
         # @!method before_initialize
 
-        # Registers the given block as a callback to run after main body of {Component#initialize}.
+        # Adds given block as callback to run after main body of {Component#initialize}.
+        # These are hereditary, running in **top-down order:** callbacks from ancestors first, then descendents.
         #
+        # @api private
         # @yield callback to run
         # @return [Array<Proc>] all callbacks defined using `.after_initialize` for this class
         # @!method after_initialize
 
-        # @!endgroup
-
         CALLBACK_METHODS.each do |method_sym|
           civar_sym = "@#{method_sym}_cbs".to_sym
           define_method(method_sym) do |&block|
-            if self.instance_variable_defined?(civar_sym)
-              blocks = self.instance_variable_get(civar_sym)
-            else
-              blocks = []
-            end
+            blocks = instance_variable_defined?(civar_sym) ? instance_variable_get(civar_sym) : []
             blocks << block
-            self.instance_variable_set(civar_sym, blocks)
+            instance_variable_set(civar_sym, blocks)
           end
         end
       end
 
       private
 
-      # @!group Callback Runners (Internal)
-      # @!visibility public
+      # @private
 
-      # Runs all before initialize callbacks in the component's hierarchy, in **bottom-up order**: those defined in descendents first, then ancestors
+      # Runner for callbacks added by #before_initialize.
       #
       # @api private
       # @return [void]
       # @note called at the start of {Component#initialize}
       # @!method run_before_initialize_cbs
 
-      # Runs all after initialize callbacks in the component's hierarchy, in **top-down order**: those defined in ancestors first, then descendents
+      # Runner for callbacks added by #after_initialize.
       #
       # @api private
       # @return [void]
       # @note called at the end of {Component#initialize}
       # @!method run_after_initialize_cbs
-
-      # @!endgroup
 
       CALLBACK_METHODS.each do |method_sym|
         civar_sym  = "@#{method_sym}_cbs".to_sym
@@ -104,13 +99,11 @@ module Denko
           # Need to check civars in ancestors too.
           klasses = self.class.ancestors
           # If running "after" reverse hierarchy so they run top-down.
-          klasses = klasses.reverse if method_sym.to_s.start_with? "after"
+          klasses = klasses.reverse if method_sym.to_s.start_with? 'after'
 
           blocks = []
           klasses.each do |klass|
-            if klass.instance_variable_defined?(civar_sym)
-              blocks << klass.instance_variable_get(civar_sym)
-            end
+            blocks << klass.instance_variable_get(civar_sym) if klass.instance_variable_defined?(civar_sym)
           end
           blocks = blocks.flatten
           blocks.each { |b| instance_exec(&b) }
