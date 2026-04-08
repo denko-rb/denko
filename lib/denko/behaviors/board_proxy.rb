a3ea119a226f6c6f5d8d54de4738990b8b77c863
vickash <vickashmahabir@gmail.com>
2026-04-08 19:13:11 -0400

    docs : document Behaviors::BoardProxy

--- lib/denko/behaviors/board_proxy.rb
+++ lib/denko/behaviors/board_proxy.rb
@@ -1,40 +1,93 @@
 module Denko
   module Behaviors
+    #
+    # Provides a **bare-minimum** interface matching {Board}, so peripheral instances may
+    # transparently use a `BoardProxy` (eg. a shift register) in place of a `Board`.
+    #
+    # @example LED through a shift register
+    #   # Board, SPI bus, and output shift register.
+    #   board = Denko::Board.new(Denko::Connection::Serial.new)
+    #   bus = Denko::SPI::BitBang.new(board: board, pins: { clock: 13, output: 11 })
+    #   register = Denko::SPI::OutputRegister.new(bus: bus, pin: 10)
+    #
+    #   # LED connected to register's parallel output pin 2.
+    #   led = Denko::LED.new(board: register, pin: 2)
+    #
+    #   # OutputRegister defines #digital_write, so this works.
+    #   led.blink 0.5
+    #   sleep
+    #
     module BoardProxy
       include Subcomponents
 
+      # @return [Symbol] platform identifier (default `:proxy`)
       def platform
         :proxy
       end
 
+      # @return [Integer] always returns 1
       def high
         1
       end
 
+      # @return [Integer] always returns 0
       def low
         0
       end
 
+      # Should be implemented to return maximum ADC value (eg. 1023 for 10-bit resolution).
+      #
+      # @abstract
+      # @return [Integer] maximum ADC value
       def analog_read_high
-        nil
+        raise NotImplementedError, "#analog_read_high not implemented for #{self.class}"
       end
 
+      # Should be implemented to return maximum PWM or DAC value (eg. 255 for 8-bit resolution).
+      #
+      # @abstract
+      # @return [Integer] maximum PWM or DAC value
       def analog_write_high
-        nil
+        raise NotImplementedError, "#analog_write_high not implemented for #{self.class}"
       end
 
-      alias :pwm_high :analog_write_high
-      alias :dac_high :analog_write_high
-      alias :adc_high :analog_read_high
+      # @return [Integer] alias of {#analog_read_high}
+      def adc_high
+        analog_read_high
+      end
 
+      # @return [Integer] alias of {#analog_write_high}
+      def pwm_high
+        analog_write_high
+      end
+
+      # @return [Integer] alias of {#analog_write_high}
+      def dac_high
+        analog_write_high
+      end
+
+      # Converts a pin identifier to integer by calling `#to_i` on it by default.
+      #
+      # @param pin [Object] the pin identifier to convert
+      # @return [Integer] the pin as an integer
       def convert_pin(pin)
         pin.to_i
       end
 
-      def set_pin_mode(pin, mode, options={}); end
+      # Stub method that may be overriden with custom behavior to set a pin's I/O mode.
+      #
+      # @abstract
+      # @param pin [Integer] the pin to configure
+      # @param mode [Symbol] the mode to set
+      # @param options [Hash] additional options
+      def set_pin_mode(pin, mode, options = {}); end
 
-      def start_read; end
-
+      # Should be implemented to set a bit (pin) in instance variable storage,
+      # without writing to hardware.
+      #
+      # @abstract
+      # @param pin [Integer] the pin to modify
+      # @param value [Integer] the value to set
       def bit_set(pin, value)
         raise NotImplementedError, "#bit_set not implemented for #{self.class}"
       end
--- test/behaviors/board_proxy_test.rb
+++ test/behaviors/board_proxy_test.rb
@@ -3,6 +3,9 @@ require_relative '../test_helper'
 class ProxiedComponent
   include Denko::Behaviors::Component
   include Denko::Behaviors::BoardProxy
+
+  def analog_read_high; end
+  def analog_write_high; end
 end
 
 class BoardProxyTest < Minitest::Test
@@ -13,12 +16,19 @@ class BoardProxyTest < Minitest::Test
   def part
     @part ||= ProxiedComponent.new(board: board)
   end
-  
+
   def test_methods
     assert_equal part.high, 1
     assert_equal part.low, 0
     assert_equal part.convert_pin("7"), 7
     part.set_pin_mode(1, :output)
-    part.start_read
+  end
+
+  def test_aliases_after_overrides
+    part.analog_read_high
+    part.analog_write_high
+    part.adc_high
+    part.dac_high
+    part.pwm_high
   end
 end
